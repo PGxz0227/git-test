d9d85df225fc355bbde7a04fa50453c25039b95f
gxz <gxz@atguigu.com>
2021-10-05 23:37:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Test.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName Test
+ * @Description
+ * @Author Gxz
+ * @DateTime 2021-10-05 23:33
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
